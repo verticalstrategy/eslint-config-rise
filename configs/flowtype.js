73b468ace1e37e6959b718950a861a19b8ec3331
Steffen Agger <steffen.agger@gmail.com>
2018-01-18 12:37:49 +0100

    consistency, single extends = string

--- configs/flowtype.js
+++ configs/flowtype.js
@@ -1,6 +1,6 @@
 module.exports = {
   parser: 'babel-eslint', // ESLint parser does not support type annotations.
-  extends: ['plugin:flowtype/recommended'],
+  extends: 'plugin:flowtype/recommended',
   plugins: ['flowtype'],
   settings: {
     flowtype: {
